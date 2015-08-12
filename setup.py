2abd75b86b51d50653a940cc0c3bae24de49a784
Andreas Ruppen <andreas.ruppen@gmail.com>
2015-08-12 14:44:10 +0200

    Increased version number for pypi release

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def read(fname):
 
 setup(
     name="seriesManagementSystem",
-    version="1.1.1",
+    version="1.1.2",
     author="Andreas Ruppen",
     author_email="andreas.ruppen@gmail.com",
     description="Manages Series",
