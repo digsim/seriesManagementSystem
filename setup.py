7adf5c1e2fbc04efe4aca3046e2b305dd83e3e68
Andreas Ruppen <andreas.ruppen@gmail.com>
2015-10-07 19:15:05 +0200

    Raised version number to 1.1.5

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def read(fname):
 
 setup(
     name="seriesManagementSystem",
-    version="1.1.4",
+    version="1.1.5",
     author="Andreas Ruppen",
     author_email="andreas.ruppen@gmail.com",
     description="Manages Series",
