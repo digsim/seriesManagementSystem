2e14a942bd1043953cc7df1b401bb8bddf16cb0e
Andreas Ruppen <andreas.ruppen@adnovum.ch>
2016-11-26 17:47:59 +0100

    Make tests actually work. Even though we only have a dummy test

--- seriesmgmtsystem/sms/tests/calendarEvent_test.py
+++ seriesmgmtsystem/sms/tests/calendarEvent_test.py
@@ -7,8 +7,12 @@ class TestCalendarEvent(TestCase):
     def test_JSON(self):
         # Like this we can get access to test resources
         cwd = os.path.dirname(os.path.abspath(__file__))
-        self.calendarfile = os.path.join(cwd, 'resources/basic.ics')
-        sms = SMS();
+        calendarfile = os.path.join(cwd, 'resources/basic.ics')
+        updateBibTex = False
+        keepUnzipped = False
+        keepTemp = False
+        doZip  = False
+        sms = SMS(updateBibTex, keepUnzipped, keepTemp, doZip);
         expected="True"
         self.assertEqual("True", expected, 'There are differences')
         
