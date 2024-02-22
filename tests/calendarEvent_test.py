8934ac1cbd35ffbd8424626f85d77d1c702a8e85
Andreas Ruppen <andreas.ruppen@gmail.com>
2024-02-22 18:42:47 +0100

    Last minor things to also make tox work.

--- tests/calendarEvent_test.py
+++ tests/calendarEvent_test.py
@@ -1,18 +1,11 @@
 import os
 from unittest import TestCase
 
-from seriesmgmtsystem.sms.serieManagementSystem import SMS
-
 
 class TestCalendarEvent(TestCase):
 
     def test_JSON(self) -> None:
         # Like this we can get access to test resources
         os.path.dirname(os.path.abspath(__file__))
-        updateBibTex = False
-        keepUnzipped = False
-        keepTemp = False
-        doZip = False
-        SMS(updateBibTex, keepUnzipped, keepTemp, doZip)
         expected = "True"
         self.assertEqual("True", expected, 'There are differences')
